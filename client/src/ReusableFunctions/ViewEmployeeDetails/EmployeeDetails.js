8c82c66bbfcc7d5127774a33bc0b4821f43d7270
jamesryanpagal <ryanpagal02@gmail.com>
2021-11-14 19:02:34 +0800

    department

--- client/src/ReusableFunctions/ViewEmployeeDetails/EmployeeDetails.js
+++ client/src/ReusableFunctions/ViewEmployeeDetails/EmployeeDetails.js
@@ -16,7 +16,7 @@ import ProfileImage from "../ProfileImage/ProfileImage";
 import "./EmployeeDetails.css";
 
 // SOCKET CONNECTION
-const socket = io.connect("http://localhost:8080/");
+const socket = io.connect("https://grandspan.herokuapp.com/");
 
 const EmployeeDetails = ({ toggleViewDetails, setToggleViewDetails, id }) => {
   // --------------------------------- STATE -----------------------------
